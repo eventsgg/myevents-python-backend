5c5bbadbf67173d6a59919d52ee83612f6f2a9f8
cherninely <cherninely@yandex-team.ru>
2018-10-25 00:41:32 +0300

    frontend initial

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# python-backend
+# My Events
---
+++ front/src/components/page/Page.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Page.css';
+
+class Page extends React.Component {
+  public render() {
+    return (
+      <div className="Page">
+        'asdsadasd'
+      </div>
+    );
+  }
+}
+
+export default Page;
