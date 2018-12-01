26171fb7c47a5801cc735ee7ef67e7d66fa23af0
cherninely <cherninely@gmail.com>
2018-12-01 16:56:36 +0300

    Перенес все в модуль, откатил eject

---
+++ front/src/Components/Page/Page.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './Page.module.scss';
+
+class Page extends Component {
+  render() {
+    return (
+      <div className={styles.page}>loool</div>
+    );
+  }
+}
+
+export default Page;
