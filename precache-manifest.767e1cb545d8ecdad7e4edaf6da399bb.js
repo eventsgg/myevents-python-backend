0e6367f79ee49ee17015a64aacdd3784150d404a
cherninely <cherninely@gmail.com>
2019-02-02 14:05:22 +0300

    Updates

---
+++ precache-manifest.767e1cb545d8ecdad7e4edaf6da399bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60114f9b08328f6eaced",
+    "url": "/myevents/static/js/runtime~main.60114f9b.js"
+  },
+  {
+    "revision": "9221942081e0726e2641",
+    "url": "/myevents/static/js/main.92219420.chunk.js"
+  },
+  {
+    "revision": "9fc86c70efa3fef80670",
+    "url": "/myevents/static/js/1.9fc86c70.chunk.js"
+  },
+  {
+    "revision": "9221942081e0726e2641",
+    "url": "/myevents/static/css/main.29fdc92b.chunk.css"
+  },
+  {
+    "revision": "9d30de4665e82d70dda0c4c41471ffcc",
+    "url": "/myevents/index.html"
+  }
+];
