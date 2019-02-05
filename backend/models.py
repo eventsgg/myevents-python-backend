fde624af94a60a90966e4a3013bdc151262cfe49
phDooY <david91.05@mail.ru>
2019-02-05 20:28:29 +0300

    models updated due to last data model design

--- backend/models.py
+++ backend/models.py
@@ -1,39 +1,142 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, func
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Boolean, Float, func
 from sqlalchemy.orm import backref, relationship
+import enum
 
 from database import Base
+from datetime import datetime
 
 
-class Department(Base):
-    __tablename__ = 'department'
+class Event(Base):
+    __tablename__ = 'event'
     id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
+    updated_at = Column(DateTime(timezone=True))
     name = Column(String)
 
-
-class Role(Base):
-    __tablename__ = 'roles'
-    role_id = Column(Integer, primary_key=True)
-    name = Column(String)
-
-
-class Employee(Base):
-    __tablename__ = 'employee'
+class EventMedia(Base):
+    __tablename__ = 'event_media'
     id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
+    updated_at = Column(DateTime(timezone=True))
+    event_id = Column(Integer, ForeignKey('event.id'))
+    url = Column(String)
+    title = Column(String)
+    event = relationship(
+        Event,
+        backref=(
+            'event',
+            uselist=True,
+            cascade='delete,all'
+        )
+    )
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
+    updated_at = Column(DateTime(timezone=True))
+    username = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+
+class UserMedia(Base):
+    __tablename__ = 'user_media'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
+    updated_at = Column(DateTime(timezone=True))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    url = Column(String)
+    title = Column(String)
+    user = relationship(
+        User,
+        backref=(
+            'user',
+            uselist=True,
+            cascade='delete,all'
+        )
+    )
+
+class Share(Base):
+    __tablename__ = 'share'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
+    updated_at = Column(DateTime(timezone=True))
+    event_id = Column(Integer, ForeignKey('event.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    social_network_id = Column(Integer, ForeignKey('social_network.id'))
+    user = relationship(
+        User,
+        backref=(
+            'user',
+            uselist=True,
+            cascade='delete,all'
+        )
+    )
+    event = relationship(
+        Event,
+        backref=(
+            'event',
+            uselist=True,
+            cascade='delete,all'
+        )
+    )
+    social_network = relationship(
+        SocialNetwork,
+        backref=(
+            'social_network',
+            uselist=True,
+            cascade='delete,all'
+        )
+    )
+
+class SocialNetwork(Base):
+    __tablename__ = 'social_network'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
+    updated_at = Column(DateTime(timezone=True))
     name = Column(String)
-    # Use default=func.now() to set the default hiring time
-    # of an Employee to be the current time when an
-    # Employee record was created
-    hired_on = Column(DateTime, default=func.now())
-    department_id = Column(Integer, ForeignKey('department.id'))
-    role_id = Column(Integer, ForeignKey('roles.role_id'))
-    # Use cascade='delete,all' to propagate the deletion of a Department onto its Employees
-    department = relationship(
-        Department,
-        backref=backref('employees',
-                        uselist=True,
-                        cascade='delete,all'))
-    role = relationship(
-        Role,
-        backref=backref('roles',
-                        uselist=True,
-                        cascade='delete,all'))
+
+class Address(Base):
+    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
+    updated_at = Column(DateTime(timezone=True))
+    city_id = Column(Integer, ForeignKey('city.id'))
+    zipcode = Column(String)
+    street_name = Column(String)
+    house_number = Column(String)
+    comment = Column(String)
+    geo_latitude = Column(postgresql.String(30))
+    geo_longitude = Column(postgresql.String(30))
+    city = relationship(
+        City,
+        backref=(
+            'city',
+            uselist=True,
+            cascade='delete,all'
+        )
+    )
+
+class City(Base):
+    __tablename__ = 'city'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
+    updated_at = Column(DateTime(timezone=True))
+    name = Column(String)
+    country_id = Column(Integer, ForeignKey('country.id'))
+    country = relationship(
+        Country,
+        backref=(
+            'country',
+            uselist=True,
+            cascade='delete,all'
+        )
+    )
+
+class Country(Base):
+    __tablename__ = 'country'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
+    updated_at = Column(DateTime(timezone=True))
+    name = Column(Integer)
+    iso_code = Column(String(length=2))
