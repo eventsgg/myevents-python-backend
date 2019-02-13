fa5321520b449b17fde43bceb590e0bcccd5ec72
cherninely <cherninely@gmail.com>
2019-02-14 01:09:13 +0300

    Сделал красивую поделяшку

--- frontend/src/Components/ShareModal/ShareModal.tsx
+++ frontend/src/Components/ShareModal/ShareModal.tsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
+
 import { withStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+
+import instagramIcon from '../../Assets/Social-media/instagram.svg';
+import facebookIcon from '../../Assets/Social-media/facebook.svg';
+import twitterIcon from '../../Assets/Social-media/twitter.svg';
 
 interface IShareModalProps {
     shareModalStore: {
@@ -17,32 +23,45 @@ interface IShareModalProps {
 }
 
 @observer class PureShareModal extends Component<IShareModalProps> {
-    close() {
+    close = () => {
         this.props.shareModalStore.open = false;
     }
 
     render() {
         const { classes } = this.props;
+        const socialMedia = [
+            instagramIcon,
+            facebookIcon,
+            twitterIcon,
+        ];
 
         return (
             <Dialog
-                open={this.props.shareModalStore.open} 
-                onClose={this.close.bind(this)}
+                open={this.props.shareModalStore.open}
+                onClose={this.close}
+                fullWidth={true}
             >
                 <DialogTitle disableTypography className={classes.titleRoot}>
-                    <Typography variant="h6">Расшарь, слыш?</Typography>
-                    <IconButton className={classes.closeButtonRoot} onClick={this.close.bind(this)}>
-                        <CloseIcon/>
+                    <Typography variant="h6">Расшарь пост в социальной сети и получи большую скидку</Typography>
+                    <IconButton className={classes.closeButtonRoot} onClick={this.close}>
+                        <CloseIcon />
                     </IconButton>
                 </DialogTitle>
 
                 <DialogContent>
-                    <DialogContentText>
-                        Соц сеточка 1, соц сеточка2
-                        Соц сеточка 1, соц сеточка2
-                        Соц сеточка 1, соц сеточка2
-                        Соц сеточка 1, соц сеточка2
-                    </DialogContentText>
+                    <Grid container spacing={32} wrap="nowrap">
+                        {
+                            socialMedia.map((icon, i) => {
+                                return (
+                                    <Grid item xs={12} key={i}>
+                                        <Link target="_blank" href={`${icon}`}>
+                                            <img src={icon} />
+                                        </Link>
+                                    </Grid>
+                                )
+                            })
+                        }
+                    </Grid>
                 </DialogContent>
 
             </Dialog>
