e6d8b37495c3f7393c708e5e1770c2b43dda41c1
phDooY <david91.05@mail.ru>
2019-05-16 19:22:37 +0300

    main view renders social accounts URLs dynamically

--- backend/myevents/myevents/views.py
+++ backend/myevents/myevents/views.py
@@ -2,26 +2,17 @@ from django.views.generic.base import TemplateView
 from django.utils.safestring import mark_safe
 import json
 
-SOCIAL_URLS = [
-    '/accounts/instagram/login/?process=login', 
-    '/accounts/facebook/login/?process=login', 
-    '/accounts/vk/login/?process=login'
-    ]
-
-## Non-working dynamic URL invocation
-# import allauth.socialaccount
-
-# pl = allauth.socialaccount.providers.registry.get_list()
-
-# SOCIAL_URLS = []
-
-# for i in pl:
-#     SOCIAL_URLS.append(i.get_login_url(i.request, process='login'))
 
 class Index(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, *args, **kwargs):
+        import allauth.socialaccount
+        SOCIAL_URLS = []
+        pl = allauth.socialaccount.providers.registry.get_list()
+        for i in pl:
+            SOCIAL_URLS.append(i.get_login_url(i.request, process='login'))
+
         context = super(Index, self).get_context_data(*args, **kwargs)
         context['data'] = mark_safe(json.dumps(
             {
@@ -31,3 +22,46 @@ class Index(TemplateView):
             }
         ))
         return context
+
+
+## Examples taken from https://github.com/mirumee/saleor/blob/master/tests/api/test_core.py
+
+## GraphQL Query
+#     query = """
+#     query {
+#         products {
+#             edges {
+#                 node {
+#                     name
+#                 }
+#             }
+#         }
+#     }
+#     """
+#     response = api_client.post_graphql(query)
+#     content = _get_graphql_content_from_response(response)
+
+## GraphQL Mutation
+# # costPrice is snake case variable (cost_price) in the backend
+#     query = """
+#     mutation testCamel($id: ID!, $cost: Decimal) {
+#         productVariantUpdate(id: $id,
+#         input: {costPrice: $cost, trackInventory: false}) {
+#             errors {
+#                 field
+#                 message
+#             }
+#             productVariant {
+#                 id
+#             }
+#         }
+#     }
+#     """
+#     variables = {
+#         "id": graphene.Node.to_global_id("ProductVariant", variant.id),
+#         "cost": 12.1234,
+#     }
+#     response = staff_api_client.post_graphql(
+#         query, variables, permissions=[permission_manage_products]
+#     )
+#     content = get_graphql_content(response)
