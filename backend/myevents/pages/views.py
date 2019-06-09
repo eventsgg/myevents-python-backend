6f0a3e2554343b9227da8c7baeaef40b83340cba
cherninely <cherninely@gmail.com>
2019-06-09 12:19:52 +0300

    Удалить бэк на python

--- backend/myevents/pages/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import View
-from django.shortcuts import render
-
-
-class FrontendRenderView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'pages/index.html', {})
